ae15f150979d8263a85c74f5481b16fa7f318be7
Ranjan Shrestha <ranjan.shrestha.np@gmail.com>
2025-02-21 16:02:01 +0545

    Use uv; use scheduler to pull file; few fixes

--- Dockerfile
+++ Dockerfile
@@ -1,24 +1,33 @@
 FROM python:3.10-slim-buster
 
-LABEL maintainer="Togglecorp Dev"
+COPY --from=ghcr.io/astral-sh/uv:0.5.29 /uv /uvx /bin/
 
-ENV PYTHONUNBUFFERED 1
+LABEL maintainer="Montandon Dev"
+LABEL org.opencontainers.image.source="https://github.com/IFRCGo/geocoding-service/"
+
+ENV PYTHONUNBUFFERED=1
+
+ENV UV_COMPILE_BYTECODE=1
+
+ENV UV_LINK_MODE=copy
+
+ENV UV_PROJECT_ENVIRONMENT="/usr/local/"
 
 WORKDIR /code
 
-RUN apt-get update -y && \
-    apt install -y gdal-bin libgdal-dev && \
-    rm -rf /var/lib/apt/lists/*
-
-COPY pyproject.toml poetry.lock /code/
-
-# Upgrade pip and install python packages for code
-RUN pip install --upgrade --no-cache-dir pip poetry \
-    && poetry --version \
-    # Configure to use system instead of virtualenvs
-    && poetry config virtualenvs.create false \
-    && poetry install --no-root \
-    # Remove installer
-    && pip uninstall -y poetry virtualenv-clone virtualenv
+RUN --mount=type=cache,target=/root/.cache/uv \
+    --mount=type=bind,source=uv.lock,target=uv.lock \
+    --mount=type=bind,source=pyproject.toml,target=pyproject.toml \
+    apt-get update -y \
+    && apt-get install -y --no-install-recommends \
+        # Build required packages
+        gcc libc-dev gdal-bin libproj-dev \
+        # Helper packages
+        wait-for-it \
+    && uv sync --frozen --no-install-project  --no-dev \
+    # Clean-up
+    && apt-get remove -y gcc libc-dev libproj-dev \
+    && apt-get autoremove -y \
+    && rm -rf /var/lib/apt/lists/*
 
 COPY . /code/
--- download.py
+++ download.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import shutil
 import zipfile
 from typing import Optional
 
@@ -11,23 +12,38 @@ logger.setLevel(logging.INFO)
 
 URL = "https://files.emdat.be/data/gaul_gpkg_and_license.zip"
 
+GPKG_DIR_BASE_PATH = os.getenv("GPKG_DIR_BASE_PATH")
 
-def check_and_download_gaul_file(filename: str = "gaul_gpkg.zip", chunk_size: int = 128, timeout: int = 30) -> Optional[str]:
+
+def check_and_download_gaul_file(
+    filename: str = "gaul_gpkg.zip", chunk_size: int = 128, timeout: int = 30, scheduler_trigger: bool = False
+) -> Optional[str]:
     """
     Checks and Downloads the gaul file
     if it doesn't exist and returns the file path
     """
     zip_file_path = f"/tmp/{filename}"
-    files_extraction_path = "/code/extracted_files"
-    gaul_file_path = "/code/extracted_files/gaul2014_2015.gpkg"
+    gaul_file_path = f"{GPKG_DIR_BASE_PATH}/gaul2014_2015.gpkg"
+
+    if scheduler_trigger:
+        try:
+            shutil.rmtree(GPKG_DIR_BASE_PATH)
+        except OSError as e:
+            logger.error(f"Could not delete the directory {GPKG_DIR_BASE_PATH} with error {e}")
+
+    if not os.path.isdir(GPKG_DIR_BASE_PATH):
+        os.makedirs(GPKG_DIR_BASE_PATH)
 
     if os.path.exists(gaul_file_path):
         logger.info("The file already exists in the path.")
         return gaul_file_path
 
     logging.info("File Download has started.")
+    try:
+        response = requests.get(url=URL, stream=True, timeout=timeout)
+    except (requests.Timeout, requests.ReadTimeout) as e:
+        raise Exception("Timout occurred while downloading the zip gpkg file. %s", e)
 
-    response = requests.get(url=URL, stream=True, timeout=timeout)
     if response.status_code == 200:
         with open(zip_file_path, "wb") as file:
             for chunk in response.iter_content(chunk_size=chunk_size):
@@ -36,7 +52,7 @@ def check_and_download_gaul_file(filename: str = "gaul_gpkg.zip", chunk_size: in
         logger.info("Extracting zip contents.")
         try:
             with zipfile.ZipFile(zip_file_path, "r") as zip_ref:
-                zip_ref.extractall(files_extraction_path)
+                zip_ref.extractall(GPKG_DIR_BASE_PATH)
         except zipfile.BadZipFile:
             logger.error("Couldn't extract the zip contents.")
             return None
