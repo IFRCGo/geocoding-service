cfc9d31cc3bb1caa5c0375671c0bbebaf0217373
Ranjan Shrestha <ranjan.shrestha.np@gmail.com>
2025-08-19 15:55:46 +0545

    Include the low resolution geometry option

--- config.py
+++ config.py
@@ -10,6 +10,12 @@ class Settings(BaseSettings):
     # EMDAT GAUL
     GAUL_FILE_PATH: str = "./geodata-prep/geodata/gaul.gpkg"
     GAUL_DOWNLOAD_URL: str = "https://github.com/IFRCGo/geocoding-service/releases/download/v1.0.0/gaul.gpkg"
+    # World Administrative Boundaries
+    SUPER_SIMPLIFIED_WAB_FILE_PATH: str = "./geodata-prep/geodata/super_simple.wab.fgb"
+    SUPER_SIMPLIFIED_WAB_DOWNLOAD_URL: str = "https://github.com/IFRCGo/geocoding-service/releases/download/v1.0.0/wab.fgb"
+    # EMDAT GAUL
+    SUPER_SIMPLIFIED_GAUL_FILE_PATH: str = "./geodata-prep/geodata/super_simple.gaul.gpkg"
+    SUPER_SIMPLIFIED_GAUL_DOWNLOAD_URL: str = "https://github.com/IFRCGo/geocoding-service/releases/download/v1.0.0/gaul.gpkg"
 
 
 settings = Settings()
--- geodata-prep/prepare.sh
+++ geodata-prep/prepare.sh
@@ -4,6 +4,7 @@ set -e
 set -x
 
 echo "Simplification tolerance is $TOLERANCE"
+echo "Super Simplification tolerance is $HIGH_TOLERANCE"
 echo "Data directory is at $DATA_DIR"
 
 # PROCESS EMDAT GAUL
@@ -13,6 +14,7 @@ ZIP_NAME="gaul.zip"
 ITEM_NAME="gaul2014_2015.gpkg"
 FILE_NAME="gaul.gpkg"
 SIMPLIFIED_FILE_NAME="simple.$FILE_NAME"
+SUPER_SIMPLIFIED_FILE_NAME="super_simple.$FILE_NAME"
 
 # Initialize
 mkdir -p "$TMP_DIR"
@@ -25,11 +27,13 @@ unzip -u "$TMP_DIR/$ZIP_NAME" "$ITEM_NAME" -d "$TMP_DIR"
 
 # Simplify GAUL file while preserving the topology
 ogr2ogr "$TMP_DIR/$SIMPLIFIED_FILE_NAME" "$TMP_DIR/$ITEM_NAME" -simplify $TOLERANCE
+ogr2ogr "$TMP_DIR/$SUPER_SIMPLIFIED_FILE_NAME" "$TMP_DIR/$ITEM_NAME" -simplify $HIGH_TOLERANCE
 # QT_QPA_PLATFORM=offscreen qgis_process plugins enable grassprovider
 # QT_QPA_PLATFORM=offscreen qgis_process run grass7:v.generalize --input="$TMP_DIR/$ITEM_NAME" --output="$TMP_DIR/$SIMPLIFIED_FILE_NAME" --threshold=0.01 --type=1 --method=0 --error="$TMP_DIR/errors.qgis.log"
 
 # Cleanup
 mv "$TMP_DIR/$SIMPLIFIED_FILE_NAME" "$DATA_DIR"
+mv "$TMP_DIR/$SUPER_SIMPLIFIED_FILE_NAME" "$DATA_DIR"
 
 # PROCESS WAL
 
@@ -39,6 +43,7 @@ DATA_DIR=/geodata
 TMP_DIR="$DATA_DIR/tmp_wab"
 FILE_NAME="wab.fgb"
 SIMPLIFIED_FILE_NAME="simple.$FILE_NAME"
+SUPER_SIMPLIFIED_FILE_NAME="super_simple.$FILE_NAME"
 
 # Initialize
 mkdir -p "$TMP_DIR"
@@ -48,6 +53,8 @@ curl --no-progress-meter --output "$TMP_DIR/$FILE_NAME" "https://public.opendata
 
 # Simplify GAUL file while preserving the topology
 ogr2ogr "$TMP_DIR/$SIMPLIFIED_FILE_NAME" "$TMP_DIR/$FILE_NAME" -simplify $TOLERANCE
+ogr2ogr "$TMP_DIR/$SUPER_SIMPLIFIED_FILE_NAME" "$TMP_DIR/$FILE_NAME" -simplify $HIGH_TOLERANCE
 
 # Cleanup
 mv "$TMP_DIR/$SIMPLIFIED_FILE_NAME" "$DATA_DIR"
+mv "$TMP_DIR/$SUPER_SIMPLIFIED_FILE_NAME" "$DATA_DIR"
